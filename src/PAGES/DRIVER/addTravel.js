546defc5a670a87fa82c5211bd3de1ef5f314482
mmfo <m0583267055@gmail.com>
2022-12-09 00:48:24 +0200

    create basic pages with router

---
+++ src/PAGES/DRIVER/addTravel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function AddTravel() {
+    return (
+        <>
+            <h1>AddTravel</h1>
+        </>
+    )
+}
+
---
+++ src/PAGES/PASSENGER/travelSearch.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function TravelSearch() {
+    return (
+        <>
+            <h1>TravelSearch</h1>
+        </>
+    )
+}
+
